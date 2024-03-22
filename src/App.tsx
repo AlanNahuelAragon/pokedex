6adabac629d8bdc805fe3e41b13e16f0349e7051
AlanNahuelAragon <alan.aragon.exe@gmail.com>
2024-03-22 15:26:04 -0300

    not found flow modified, not found page deleted

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,14 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Pokemon from './pages/Pokemon'
 import Pokemons from './pages/Pokemons'
-import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="pokedex/pokemon/:name" element={<Pokemon />} />
-        <Route path="pokedex/pokemons" element={<Pokemons />} />
+        <Route path="pokedex/pokemons/:name?" element={<Pokemons />} />
         <Route path="pokedex/" element={<Pokemons />} />
-        <Route path='pokedex/pokemonnotfound' element={<NotFound/>} />
       </Routes>
     </Router>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import styles from "./header.module.css"
 import pbLogo from "../assets/PokeLogoW.png"
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 type HeaderProps ={
     query:string,
@@ -8,11 +9,19 @@ type HeaderProps ={
 }
 
 const Header = ({query,setQuery}:HeaderProps) => {
+    const {name} : {name?:string} = useParams();
     const navigate = useNavigate();
+    useEffect(()=>{
+        if(name){
+            setQuery(name);
+        }
+    },[name,setQuery])
+
     const handleSubmit = ()=>{
-        navigate(`/pokedex/pokemon/${query.toLowerCase()}`)
+        navigate(`/pokedex/pokemons/${query.toLowerCase()}`)
     }
     const backToMain = ()=>{
+        setQuery("")
         navigate(`/pokedex/pokemons/`)
     }
 
